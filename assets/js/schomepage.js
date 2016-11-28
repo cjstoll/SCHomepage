1eefccbf0fb3480a255cfa1c68ab9c6c061cdc0c
Chris <christopher.j.stoll@gmail.com>
2016-11-28 14:36:53 -0500

    Updated to use the 'overlay' vs 'dropdown' selects

--- assets/js/schomepage.js
+++ assets/js/schomepage.js
@@ -10,13 +10,18 @@
 
 var scApp  =  angular.module('scApp', []);
 
-scApp.controller('scController', ['$scope', '$http', function($scope, $http) { 
+scApp.controller('scController', ['$scope', '$http', '$q', function($scope, $http, $q) { 
     $scope.master = {};
 	$scope.nlform;
 	//<select> Variables
-	$scope.selSolution = 'solutions';
-	$scope.selIndustry = '';
-	$scope.selProduct = '';
+	$scope.selSolution = 'solution';
+	$scope.selIndustry = 'any industry';
+	$scope.selProduct = 'any product';
+	$scope.nlFieldOpen = {
+		solutions: false,
+		industry: false,
+		products: false
+	};
 	
 	//Arrays for holding the results that will populate the selects and/or results
 	$scope.industry = [];
@@ -27,9 +32,19 @@ scApp.controller('scController', ['$scope', '$http', function($scope, $http) {
 	//Dataset for the first dropdown 'selSolution'
 	$scope.solutionTypes = [{ id:"solutions", name:"solution"},{ id:"wins", name:"winning solution"}];
 	
+	
+	$scope.showFieldOpen = function(field) {
+		$scope.nlFieldOpen[field] = !$scope.nlFieldOpen[field];
+	}
 	//Update the <select> for chosing the Industry based on the options available in the appropriate dataset
 	//The dataset must be reduced to a unique collection so that the user only sees one choice in the dropdown 
-    $scope.updateIndustry = function() {
+    $scope.updateIndustry = function(id) {
+		
+		if(id=='wins'){
+			$scope.selSolution = 'winning solution';
+		}else{
+			$scope.selSolution = 'solution';
+		}
 		//Hide the results div
 		$('#resultsLine').hide();
 		$('#resultsData').hide();
@@ -39,15 +54,19 @@ scApp.controller('scController', ['$scope', '$http', function($scope, $http) {
 		$scope.selProduct = 'any product';
 		
 		//Get the working dataset based on the selection and populate the industry <select>
-		$http.get('assets/json/'+$scope.selSolution+'.json').success(function(data){ 
+		$http.get('assets/json/'+id+'.json').success(function(data){ 
 			$scope.resultData = $scope.solutions = data;
 			$scope.industry = $scope.unique($scope.resultData,'industry'); 
 		});
+		$scope.nlFieldOpen.solutions = false;
 	};
 	
 	//Update the <select> for chosing the Products based on the Industry selected from the appropriate solution dataset
     //The dataset must be reduced to a unique collection so that the user only sees one choice in the dropdown 
-    $scope.updateProducts = function() {
+    $scope.updateProducts = function(id) {
+		
+		$scope.selIndustry = id;
+		
 		//Hide the results div
 		$('#resultsLine').hide();
 		$('#resultsData').hide();
@@ -60,11 +79,15 @@ scApp.controller('scController', ['$scope', '$http', function($scope, $http) {
 			$scope.updateIndustry();
 		}else{
 			//Filter (reduce) the current dataset to only those entries matching the selected industry
-			$scope.resultData = $scope.filter($scope.solutions,'industry',$scope.selIndustry);
+			$scope.resultData = $scope.solutions = $scope.filter($scope.solutions,'industry',$scope.selIndustry);
 			$scope.products = $scope.unique($scope.resultData,'product'); 
 		}
+		$scope.nlFieldOpen.industry = false;
 	};
-	$scope.updateResults = function() {
+	$scope.updateResults = function(id) {
+		
+		$scope.selProduct = id;
+		
 		//Hide the results div
 		$('#resultsLine').hide();
 		$('#resultsData').hide();
@@ -74,8 +97,9 @@ scApp.controller('scController', ['$scope', '$http', function($scope, $http) {
 			$scope.updateProducts();
 		}else{
 			//Filter (reduce) the current dataset to only those entries matching the selected product.
-			$scope.resultData = $scope.filter($scope.resultData,'product',$scope.selProduct);
+			$scope.resultData = $scope.filter($scope.solutions,'product',$scope.selProduct);
 		}
+		$scope.nlFieldOpen.products = false;
 	};
 
 	//When the user clicks the 'Find It!' button, show the resulting table
@@ -113,9 +137,7 @@ scApp.controller('scController', ['$scope', '$http', function($scope, $http) {
 	
 	angular.element(document).ready(function(){
 		//Since the first dropdown loads with 'solutions' make the industry dropdown populated with the right data
-		$scope.updateIndustry();
-		
-		//Could not get the 'pretty' look of the overlays to function correctly.  Still working on it.
-		//$scope.nlform = new NLForm( document.getElementById( 'nl-form' ) );
+		$scope.updateIndustry('solutions');
+		$scope.nlSolutionOpen = false;
 	});
 }]);
